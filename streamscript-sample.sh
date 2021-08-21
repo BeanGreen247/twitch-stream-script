66713e87641c65f706d8dead8172ab8661e87321
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2021-08-21 21:55:18 +0200

    Update streamscript-sample.sh

--- streamscript-sample.sh
+++ streamscript-sample.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pkill -f /dev/video0
-mpv --geometry=-0-0 --autofit=30% --untimed --cache-secs=0 --demuxer-readahead-secs=0 /dev/video0 & ffmpeg -y -f x11grab -s 1680x1050 -i :0.0 -threads 0 -f alsa -i default -f pulse -filter_complex amerge -ac 2 -i 3 -vf scale=1280x720 -c:v h264 -g 24 -b:v 2M -preset ultrafast -c:a aac -pix_fmt yuv444p -f flv "rtmp://live-lax.twitch.tv/app/xxxx_xxxxxxxxx_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ffplay -f v4l2 -noborder -an -sn -alwaysontop -video_size 380x360 /dev/video0 & ffmpeg -y -threads 4 -thread_queue_size 16 -f x11grab -s 1680x1050 -i :0.0 -f alsa -i default -vol 2000 -f pulse -i 2 -filter_complex "[1:0][2:0]amix=inputs=2:duration=shortest" -vf scale=1280x720 -g 30 -b:v 2M -tune zerolatency -keyint_min 30 -c:a aac -c:v h264_nvenc -crf 25 -pixel_format yuv444p -f flv "rtmp://live-lax.twitch.tv/app/xxxx_xxxxxxxxx_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
